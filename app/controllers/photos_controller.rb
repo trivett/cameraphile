ece55b48a57c15f7d6233230d73e36cba1122044
trivett <trivett@gmail.com>
2014-05-14 22:42:42 -0400

    not ideal, but not broken

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,7 +2,7 @@ class PhotosController < ApplicationController
 
   def show
     @photo = Photo.find(params[:id])
-    @camera = Camera.find_by(:name => @photo.camera_model)
+    @camera = Camera.find_by(:name => @photo.camera_model) || Camera.where(:name => "Unknown")
   end
 
 
