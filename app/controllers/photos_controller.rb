7e43f884b548f07b324240228abbb98d17f37615
trivett <trivett@gmail.com>
2014-05-14 22:17:39 -0400

    fixing other errors now

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,10 +3,10 @@ class PhotosController < ApplicationController
   def show
     @photo = Photo.find(params[:id])
 
-    if Camera.where(:name => @photo.camera_model).first
-      @camera = Camera.where(:name => @photo.camera_model).first
-    elsif @photo.camera
-      @camera = @photo.camera
+
+      @camera = Camera.find_by(:name => @photo.camera_model)
+
+
     else
       @camera = Camera.where(:name => "Unknown").first
     end
